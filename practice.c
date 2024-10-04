ac4f4d05bba41e973e9049e72a8302ef150810cd
krutagya.kaneria.cg@gmail.com <krutagya.kaneria.cg@gmail.com>
2024-10-04 17:37:41 +0530

    commit

--- practice.c
+++ practice.c
@@ -90,6 +90,7 @@ int main() {
 
 note1-mostly constant store karne ke lleye use karte hai jaha by default value 0 thi agae series bdegi ya first innput ke  baad
 
+
 file*var :-to create a file
 var=fopen("path","r"); :- to open that file
 char str[200]; :-read kara huva kisi variable me store karne ke leye
@@ -100,3 +101,61 @@ w=write
 r=read
 & :-adress madese
 * :- pointer ye file location me save hogi
+
+
+
+
+#include <stdio.h>
+int main(){
+    // double first=5555.55; "%lf"
+    // int secound=5;  "%d"
+    // float third=5.5;  "%f"
+    // long long four=5555555.5;  "%lf"
+    // printf("%d",sizeof(first));
+    // printf("%d",sizeof(secound));
+    // printf("%d",sizeof(third));
+    // printf("%d",sizeof(four));
+    // unsigned int num=20;
+    // printf("%u",num);
+}
+
+note1- int-4(6-7 digits) double-8(15 digits) long-8(15 digits) float-4(6-7 digits) 
+note2- un-signed - 0 se + value store kar sakte hai(0 to 256 digits ) 
+       signed - - se + value store kar sakte hai(-128 to 127 digits)
+note3-%i for-    
+      %F for-
+note4-rightshift leftshift & | ^
+note5-ternaray operater , short hand if
+ 
+function add(int num){
+  static int total;
+  total=total+num;
+  returnn total;
+}
+int main(){
+  add(3)
+  add(4)
+  add(5)
+}
+note6-static int total   static likhne ke baad total ki valu vo hi rahegi
+before static 0+3
+              3+4
+              7+5
+after static 0+3
+             0+4
+             0+5
+
+1)strcture:
+example:- strct name{
+int num;  4bits
+float two; 8 bits
+char c;  1bits
+total-: 4+8+1=13bits
+
+
+2)union:
+example:- union name{
+int num;  4bits
+float two; 8 bits
+char c;  1bits
+total-: max bits hoga vo total storage hogi means 8 bits
